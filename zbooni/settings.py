7a89e66d67e1234ad45f078081fde9263a4a2df7
Yasser Ali <ary@odoo.com>
2022-04-20 03:38:31 +0400

    [#000][ADD] adds zbooni_auth initial state and required 3rd party libs

--- zbooni/settings.py
+++ zbooni/settings.py
@@ -23,6 +23,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # Third-Party Apps
+    'rest_framework',
+
+    # apps
+    'zbooni_auth',
+
 ]
 
 MIDDLEWARE = [
